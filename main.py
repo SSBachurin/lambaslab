9e9b1d32247b70e34e5451558b69796027c0048d
Stanislav Bachurin <bachurin.rostgmu@gmail.com>
2024-01-31 22:35:12 +0300

    revised main file; method created

--- main.py
+++ main.py
@@ -9,54 +9,67 @@ from dataclasses import dataclass
 @dataclass
 class Lambas:
     yaml_file_path = ''
-    cur_dir = ''
-    geometry_folder = os.path.join(cur_dir, 'geometries')
-    protocols_folder = ''
-    results_folder = os.path.join(cur_dir, 'results')
+    home_dir = ''  # домашняя директория
+    cur_dir = ''  # Текущая рабочая папка
+    geometry_folder = os.path.join(home_dir, 'geometries')
+    # protocols_folder = ''
+    results_folder = os.path.join(home_dir, 'results')
     geometries = [f for f in os.listdir(geometry_folder)
                   if f.endswith('.pdb')]
+
     commands_list = []
-    # Перечень комманд, которые должна парсить программа из файла .yaml
     lambaslab_commands = dict()
     bufer = dict()
     work_dict = dict()
 
-    def build(self):
-        self.lambaslab_commands = {'llab_charge': self.charge}
+    def load_inner_methods(self):
+        self.lambaslab_commands = {'llab_charge': self.charge,
+                                   }
 
+    def process(self):
+        '''Запускает конвеер обработки геометрий. Сначала происходит
+        подготовка директории, затем выполняются команды в секции'''
+        for geometry in self.geometries:
+            self.build(geometry).execute()
+            os.chdir(self.home_dir)
+
+    def load_command(self, section: str):
         # Загружаем конфигурацию из YAML-файла
         with open(self.yaml_file_path, 'r') as file:
             config = yaml.safe_load(file)
         # Извлекаем последовательность команд из конфигурации
-        self.commands_list = config.get('build')
+        self.commands_list = config.get(section)
+        return self
 
-        for geometry in self.geometries:
-            print(f"BUILDING. {geometry} in process")
-            geometry_name = geometry[:-4]
-            self.work_dict['{file}'] = geometry_name
-            # Создаем папку с именем файла .pdb
-            output_folder = os.path.join(self.cur_dir,
-                                         os.path.splitext(geometry)[0])
-            os.makedirs(output_folder, exist_ok=False)
-            # Копируем файл .pdb в созданную папку
-            shutil.copy2(os.path.join(self.geometry_folder, geometry),
-                         output_folder)
-            # Переходим в созданную папку
-            os.chdir(output_folder)
-            # Выполняем команды для каждого файла .pdb
-            for command in self.commands_list:
-                if command[0] in self.lambaslab_commands.keys():
-                    self.lambaslab_commands[command[0]](*command[1:])  # тут разобраться как передать команду с параметрами корректно, видимо написать функцию parse_command(command)
-                else:
-                    # Формируем команду, добавляя имя файла .pdb
-                    for key, value in self.work_dict:
-                        command = command.replace(key, value)
-                    # command = command.replace('{file}', geometry_name).replace('{protocols_folder}', work.protocols_folder).replace('{bufer}', self.bufer)
-                    # Выполняем команду в консоли
-                    process = subprocess.Popen(command, shell=True)
-                    process.wait()
-            # Возвращаемся обратно в исходную папку
-            os.chdir(self.cur_dir)
+    def execute(self):
+        for command in self.commands_list:
+            if command[0] in self.lambaslab_commands.keys():
+                self.lambaslab_commands[command[0]](*command[1:])
+            else:
+                # Формируем команду, добавляя имя файла .pdb
+                for key, value in self.work_dict:
+                    command = command.replace(key, value)
+                # Выполняем команду в консоли
+                process = subprocess.Popen(command, shell=True)
+                process.wait()
+        # Возвращаемся обратно в исходную папку
+        os.chdir(self.home_dir)
+        return self
+
+    def build(self, geometry):
+        print(f"BUILDING. {geometry} in process")
+        geometry_name = geometry[:-4]
+        self.work_dict['{file}'] = geometry_name
+        # Создаем папку с именем файла .pdb
+        self.cur_dir = os.path.join(self.home_dir,
+                                    os.path.splitext(geometry)[0])
+        os.makedirs(self.cur_dir, exist_ok=False)
+        # Копируем файл .pdb в созданную папку
+        shutil.copy2(os.path.join(self.geometry_folder, geometry),
+                     self.cur_dir)
+        # Переходим в созданную папку
+        os.chdir(self.cur_dir)
+        return self
 
     def charge(self, substring, filename):
         with open(filename, 'r') as file:
@@ -71,8 +84,8 @@ class Lambas:
         self.work_dict['p_amount'] = charge + ion_amout
         self.work_dict['n_amount'] = ion_amout
         # Если строка с подстрокой не найдена, возвращаем None
-        return None
-
+        return self
+    
     def analyse(self):
         ...
 
@@ -95,7 +108,7 @@ if __name__ == "__main__":
     os.makedirs(os.path.join(os.getcwd(), 'results'), exist_ok=True)
 
     work = Lambas()
-    work.cur_dir = os.getcwd()
+    work.home_dir = os.getcwd()
     #work.protocols_folder = os.path.abspath(args.protocols_folder)
     work.work_dict['{protocols_folder}'] = os.path.abspath(args.protocols_folder)
     work.yaml_file_path = os.path.abspath(args.conf)
@@ -104,8 +117,10 @@ if __name__ == "__main__":
           "в рабочей директории. Удостоверьтесь, что они содержат "
           "необходимые для расчётов файлы и запустите команду build.")
 
-    exec_commands = {'build': work.build,
-                     'analyse': work.analyse,
+    exec_commands = {'build': [work.load_inner_methods,
+                               work.load_command('build'),
+                               work.process,],
+                     'analyse': [work.analyse,],
                      }
 
     inp = input('''
@@ -118,7 +133,8 @@ if __name__ == "__main__":
 
     while inp != 'quit':
         if inp in exec_commands:
-            exec_commands[inp]()
+            for i in exec_commands[inp]:
+                exec_commands[inp][i]()
             inp = input("Введите команду: ")
         else:
             print("Неизвестная команда")
