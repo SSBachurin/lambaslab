f1f55cd1a0e168438da4bf72559b7c73ffbe28ea
Stanislav Bachurin <bachurin.rostgmu@gmail.com>
2024-02-01 23:59:32 +0300

    working version

--- main.py
+++ main.py
@@ -11,11 +11,9 @@ class Lambas:
     yaml_file_path = ''
     home_dir = ''  # домашняя директория
     cur_dir = ''  # Текущая рабочая папка
-    geometry_folder = os.path.join(home_dir, 'geometries')
-    # protocols_folder = ''
-    results_folder = os.path.join(home_dir, 'results')
-    geometries = [f for f in os.listdir(geometry_folder)
-                  if f.endswith('.pdb')]
+    geometry_folder = ''
+    results_folder = ''
+    geometries = []
 
     commands_list = []
     lambaslab_commands = dict()
@@ -24,13 +22,17 @@ class Lambas:
 
     def load_inner_methods(self):
         self.lambaslab_commands = {'llab_charge': self.charge,
-                                   }
+                                   'llab_modtop': self.mod_topol,
+                                   'collect_res': self.collect_results,
+                                   'llamb_cd': self.change_dir}
+        return self
 
     def process(self):
         '''Запускает конвеер обработки геометрий. Сначала происходит
         подготовка директории, затем выполняются команды в секции'''
         for geometry in self.geometries:
-            self.build(geometry).execute()
+            self.build(geometry)
+            self.execute()
             os.chdir(self.home_dir)
 
     def load_command(self, section: str):
@@ -43,11 +45,12 @@ class Lambas:
 
     def execute(self):
         for command in self.commands_list:
-            if command[0] in self.lambaslab_commands.keys():
-                self.lambaslab_commands[command[0]](*command[1:])
+            if isinstance(command,dict):
+                func,args = command.popitem()
+                self.lambaslab_commands[func](*args)
             else:
                 # Формируем команду, добавляя имя файла .pdb
-                for key, value in self.work_dict:
+                for key, value in self.work_dict.items():
                     command = command.replace(key, value)
                 # Выполняем команду в консоли
                 process = subprocess.Popen(command, shell=True)
@@ -62,7 +65,7 @@ class Lambas:
         self.work_dict['{file}'] = geometry_name
         # Создаем папку с именем файла .pdb
         self.cur_dir = os.path.join(self.home_dir,
-                                    os.path.splitext(geometry)[0])
+                                    geometry_name)
         os.makedirs(self.cur_dir, exist_ok=False)
         # Копируем файл .pdb в созданную папку
         shutil.copy2(os.path.join(self.geometry_folder, geometry),
@@ -71,24 +74,46 @@ class Lambas:
         os.chdir(self.cur_dir)
         return self
 
-    def charge(self, substring, filename):
+    def collect_results(self, *output):
+        for o in output:
+            os.rename(self.cur_dir+'/'+o,self.results_folder+'/'+self.work_dict['{file}']+'_'+o)
+        return self
+
+    def change_dir(self, dir):
+        self.cur_dir+='/'+dir
+        os.chdir(self.cur_dir)
+        return self
+
+    # Кастомные методы работы с файлами 
+    def charge(self, filename, substring):
         with open(filename, 'r') as file:
             for line in file:
                 if line.startswith(substring):
-                    charge = int(line.strip().split()[-1])
+                    charge = int(float(line.strip().split()[-1]))
         with open('topol.top', 'r') as top:
             for line in top:
                 if line.startswith('WT4'):
                     wt_amount = int(line.strip().split()[-1])
         ion_amout = wt_amount / 34
-        self.work_dict['p_amount'] = charge + ion_amout
-        self.work_dict['n_amount'] = ion_amout
-        # Если строка с подстрокой не найдена, возвращаем None
+        self.work_dict['{p_amount}'] = str(int(ion_amout) - int(charge))
+        self.work_dict['{n_amount}'] = str(int(ion_amout))
         return self
+
+    def mod_topol(self, filename, substring, replacement):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+        with open(filename, 'w') as file:
+            for line in lines:
+                file.write(line)
+                if substring in line:
+                    file.write('\n' + replacement + '\n')
+        return self
+
+
 
     def analyse(self):
         ...
-
 
 if __name__ == "__main__":
 
@@ -96,7 +121,7 @@ if __name__ == "__main__":
         description="Пример обработки команд для lambdaslab")
 
     parser.add_argument('-c', '--conf', type=str,
-                        default="protocol.yaml",
+                        default="protocol_sirah.yaml",
                         help="Yaml протокол работы")
     parser.add_argument('-pf', '--protocols_folder', type=str,
                         default="./protocols",
@@ -109,6 +134,10 @@ if __name__ == "__main__":
 
     work = Lambas()
     work.home_dir = os.getcwd()
+    work.results_folder = work.home_dir+'/results'
+    work.geometry_folder = work.home_dir+'/geometries'
+    work.geometries = [f for f in os.listdir(work.geometry_folder)
+                  if f.endswith('.pdb')]
     work.work_dict['{protocols_folder}'] = os.path.abspath(
         args.protocols_folder)
     work.yaml_file_path = os.path.abspath(args.conf)
@@ -118,7 +147,7 @@ if __name__ == "__main__":
           "необходимые для расчётов файлы и запустите команду build.")
 
     exec_commands = {'build': [work.load_inner_methods,
-                               work.load_command('build'),
+                               lambda: work.load_command('build'),
                                work.process,],
                      'analyse': [work.analyse,],
                      }
@@ -133,8 +162,8 @@ if __name__ == "__main__":
 
     while inp != 'quit':
         if inp in exec_commands:
-            for i in exec_commands[inp]:
-                exec_commands[inp][i]()
+            for command in exec_commands[inp]:
+                command()
             inp = input("Введите команду: ")
         else:
             print("Неизвестная команда")
